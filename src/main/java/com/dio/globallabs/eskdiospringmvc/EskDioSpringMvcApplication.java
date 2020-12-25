65b2fb74d35626747a8e311bed4a3e16a598b9bb
Edson Shideki Kokado <eskokado@gmail.com>
2020-12-25 11:07:46 -0300

    Initial commit

---
+++ src/main/java/com/dio/globallabs/eskdiospringmvc/EskDioSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.dio.globallabs.eskdiospringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EskDioSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EskDioSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/globallabs/eskdiospringmvc/EskDioSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.globallabs.eskdiospringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EskDioSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
